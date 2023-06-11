9d7a1ea9081eff201cc5d9fdf3ec673b6a1d6b68
mohamad hosari <almohammad.mohe@gmail.com>
2023-06-11 23:58:50 +0300

    rating clothes

--- src/components/catagorie.vue
+++ src/components/catagorie.vue
@@ -15,6 +15,19 @@
                     <div class="img-conta">
                         <img src="../assets/images/p1.jpeg" alt="product image">
                     </div>
+                    <div class="product-content">
+                        <h6 class="name">
+                            short sleeved hoodie
+                        </h6>
+                        <div class="rating">
+                            <span class="price">
+                                32,00 $
+                            </span>
+                            <span class="icon">
+                                <img src="../assets/images/rating.png" alt="rating">
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
