02810352195c958326f0adc50fbd28c5df6ee59f
mohamad hosari <almohammad.mohe@gmail.com>
2023-06-14 22:57:30 +0300

    womens section

--- src/App.vue
+++ src/App.vue
@@ -33,4 +33,18 @@ body{
         width: 100%;
         margin: auto;
   }
+  .btn{
+    background: transparent;
+    border: none;
+    transition: .3s;
+    cursor: pointer;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+  }
+  img{
+    width: 100%;
+  }
 </style>
--- src/components/catagorie.vue
+++ src/components/catagorie.vue
@@ -3,15 +3,15 @@
         <div class="category-hear">
             <h3>WOMAN'S</h3>
             <div class="tabs">
-                <button v-for="(tab, i) in tabs" :key="tab" class="btn"
+                <div v-for="(tab, i) in tabs" :key="tab" class="tab"
                 :class = "{'active': activeIndex === i}"
                 @click="activeIndex = i"
                 >
                 {{ tab }}
-                </button>
             </div>
+            </div></div>
             <div class="product">
-                <div class="single-product">
+                <div v-for="n in 6" :key="n" class="single-product">
                     <div class="img-conta">
                         <img src="../assets/images/p1.jpeg" alt="product image">
                     </div>
@@ -30,7 +30,10 @@
                     </div>
                 </div>
             </div>
-        </div>
+            <div class="btn">
+                SEE ALL
+            </div>
+        
     </div>
 </template>
 
@@ -46,5 +49,59 @@ const activeIndex = ref(0)
 </script>
 
 <style  scoped>
+    .category-hear{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 50px;
+    }
+    .category-hear h3{
+        font-weight: 600;
+        font-style: normal;
+        font-size: 22px;
+        line-height: 25px;
+    }
+    .tabs{
+        display: flex;
+        align-items: center;
+    }
+    .tabs .tab{
+        font-weight: 600;
+        font-size: 12px;
+        line-height: 13px;
+        opacity: 0.6;
+        transition: .3s;
+        margin-right: 32px;
+        cursor: pointer;
+    }
+    .tabs .tab.active,
+    .tabs .tab:hover{
+        opacity: 1;
+    }
+    .product{
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        gap: 30px;
+    }
+    .single-product .product-content{
+        padding: 10px 0px;
 
+    }
+    .single-product .product-content h6{
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 16px;
+        opacity: 0.6;
+    }
+    .single-product .product-content .rating{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+    }
+    .single-product .rating .price{
+        font-weight: 700;
+        font-size: 14px;
+        line-height: 16px;
+    }
 </style>
